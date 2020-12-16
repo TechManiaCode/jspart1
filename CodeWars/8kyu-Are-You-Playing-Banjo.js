01279ba0c62337ff8232435779b46d5e0c75f9c4
Apogeum12 <koemirai@gmail.com>
2020-12-16 13:45:17 +0100

    Separating tasks by Part

--- CodeWars/8kyu-Are-You-Playing-Banjo.js
+++
@@ -1,9 +0,0 @@
-const areYouPlayingBanjo = name => {
-    return (name[0].toUpperCase() ==="R")?
-      `${name} plays banjo`:
-      `${name} does not play banjo`;
-  }
-
-// Sample Tests:
-Test.assertEquals(areYouPlayingBanjo("Martin"), "Martin does not play banjo");
-Test.assertEquals(areYouPlayingBanjo("Rikke"), "Rikke plays banjo");
